ee4a5736fbb3f6e9fb6042f0bd99dacef8cdd184
pablozafra90 <pablozafra90@hotmail.com>
2025-07-30 09:15:59 +0200

    error-ortografico

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -8,7 +8,7 @@ export const Quote: React.FC<QuoteProps> = ({ quote }) => {
   return (
     <div className="max-w-156 flex flex-col items-center gap-6 animate-fade-in-up">
       <h1 className="text-lg font-light! text-left w-full">
-        Lo que tu juego favorito dice de tí:
+        Lo que tu juego favorito dice de ti:
       </h1>
       <p className="text-3xl md:text-4xl font-semibold leading-snug">{quote}</p>
       <p className="w-full text-right text-xs font-light! align-baseline">
